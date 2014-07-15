b21c7975fa847eb484db0dca98c97299aed5a89e
Fredar <a.krzeminski.freda@gmail.com>
2014-07-15 19:02:04 +0200

    Add scope pills to switch through JSON responses

--- lib/json_response.rb
+++ lib/json_response.rb
@@ -3,7 +3,23 @@ require 'json'
 module JsonResponse
   def json_response(name)
     response = File.read("responses/#{name}.json")
+    vars = {
+      created_at: Time.now.utc,
+      updated_at: Time.now.utc,
+      start_at: days_from_now(7),
+      end_at: days_from_now(14)
+    }
+    vars.each do |key, value|
+      value = value.iso8601 if value.is_a?(Time)
+      value = 'null' if value.nil?
+      response = response.gsub(key.to_s.upcase, value.to_s)
+    end
+
     json = JSON.pretty_generate(JSON.parse(response))
     %Q{<pre><code class="language-javascript">#{json}</code></pre>}
+  end
+
+  def days_from_now(days)
+    Time.now + 86400 * days
   end
 end
