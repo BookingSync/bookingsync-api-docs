8ea9ec8ea0029213db6aa2435a0c5b8a1b03ae6a
Karol Galanciak <karol.galanciak@gmail.com>
2025-02-25 15:35:53 +0100

    revert the change for introducign developers.smily.com

--- content/assets/javascripts/application.js
+++ content/assets/javascripts/application.js
@@ -1,8 +1,1 @@
 //= require bootstrap-sprockets
-
-$(document).ready(function() {
-  if(window.location.href.indexOf("developers.bookingsync.com") !== -1) {
-    var redirectURL = window.location.href.replace("developers.bookingsync.com", "developers.smily.com");
-    window.location.href = redirectURL
-  }
-});
