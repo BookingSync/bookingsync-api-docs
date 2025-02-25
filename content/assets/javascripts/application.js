f22f6abb469f9d6fa6b0cb3efdb0d1d0d39a9e4b
Karol Galanciak <karol.galanciak@gmail.com>
2025-02-25 15:23:33 +0100

    redirect from bookingsync to smily domain

--- content/assets/javascripts/application.js
+++ content/assets/javascripts/application.js
@@ -1,1 +1,8 @@
 //= require bootstrap-sprockets
+
+$(document).ready({
+  if (window.location.href.indexOf("developers.bookingsync.com") !== -1) {
+    var redirectURL = window.location.href.replace("developers.bookingsync.com", "smily.bookingsync.com")
+    window.location.href = redirectURL;
+  }
+});
