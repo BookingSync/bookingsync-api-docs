50b25675fdc21410d177fc93e729d907d42e73de
Karol Galanciak <karol.galanciak@gmail.com>
2025-02-25 15:29:33 +0100

    fix JS for redirection

--- content/assets/javascripts/application.js
+++ content/assets/javascripts/application.js
@@ -1,8 +1,8 @@
 //= require bootstrap-sprockets
 
-$(document).ready({
-  if (window.location.href.indexOf("developers.bookingsync.com") !== -1) {
-    var redirectURL = window.location.href.replace("developers.bookingsync.com", "smily.bookingsync.com")
-    window.location.href = redirectURL;
+$(document).ready(function() {
+  if(window.location.href.indexOf("developers.bookingsync.com") !== -1) {
+    var redirectURL = window.location.href.replace("developers.bookingsync.com", "developers.smily.com");
+    window.location.href = redirectURL
   }
 });
